bc2e95c4f1ad29b63c7a036ea66d3d039826eb8a
asudemor <asude-bjk@hotmail.com>
2022-10-27 21:07:20 +0300

    https://www.youtube.com/watch?v=wHTVxK7c5M4&list=PLKnjBHu2xXNNkinaVhPqPZG0ubaLN63ci&index=37 kalındı.

---
+++ WriterWebSite/Controllers/RegisterController.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WriterWebSite.Controllers
+{
+    public class RegisterController : Controller
+    {
+
+        WriterManager wm = new WriterManager(new EfWriterRepository());
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(Writer writer)
+        {
+            WriterValidator wv = new WriterValidator();
+            ValidationResult results = wv.Validate(writer);
+            if (results.IsValid)
+            {
+                writer.WriterStatus = true;
+                writer.WriterAbout = "Deneme";
+                wm.WriterAdd(writer);
+                return RedirectToAction("Blog", "Blog");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+    }
+}
